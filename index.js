9d3da38a8ec54b97eb99ac66d7cc9b50d61ff970
Anthony Rurup <tcrurup@gmail.com>
2019-09-27 18:45:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ class BoardMember{
   }
   
   doCharity(){
-    return "I like to help people";
+    return "I like to help people.";
   }
   
   releasePressStatement(){
-    return "You will see great things from Scuber";
+    return "You will see great things from Scuber.";
   }
   
   sayHi(){
-    return `Hi, my name is ${this.name}.  I am from ${this.homeState}, and I was trained in ${this.training}.`;
+    return `Hi, my name is ${this.name}.I am from ${this.homeState}, and I was trained in ${this.training}.`;
   }
 }
