4dfe438f3178777ae73b0f0ce5b11fbc734054df
Anthony Rurup <tcrurup@gmail.com>
2019-09-27 18:45:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ class BoardMember{
   }
   
   veto(){
-    return "No, I must disagree"
+    return "No, I must disagree";
+  }
+  
+  approve(){
+    return "You can do that!";
+  }
+  
+  doCharity(){
+    return "I like to help people"
+  }
+  
+  releasePressStatement(){
+    return "You will see great things from Scuber"
+  }
+  
+  sayHi(){
+    return `Hi, my name is ${this.name}.  I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
